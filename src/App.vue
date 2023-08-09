98b9f365b71c801f78d6dc2fe978ce8c19b3b5c9
haopeng <haopeng138@gmail.com>
2023-08-09 20:34:08 +0200

    Add: Home Component (Introducction)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import AppNav from './components/AppNav.vue'
+import AppHome from './components/AppHome.vue';
 </script>
 
 <template>
  <header class="header scroll-header">
     <AppNav></AppNav>
   </header>
+  <main class="main">
+    <AppHome></AppHome>
+  </main>
 </template>
 
 <style lang="scss">
