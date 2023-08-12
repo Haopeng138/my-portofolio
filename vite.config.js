74061da0f6dbbfa8dec254f7ca5d3efd3c8a59b7
haopeng <haopeng138@gmail.com>
2023-08-12 21:03:57 +0200

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,32 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
- optimizeDeps: {
-    include: ["vue", "@vueuse/core"], // Add other dependencies if needed
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+    base: "https://github.com/Haopeng138/my-portofolio",
+    plugins: [vue()],
+    optimizeDeps: {
+        include: ["vue", "@vueuse/core"], // Add other dependencies if needed
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    }
+                },
+            },
         },
-      },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "${path.resolve(__dirname, './src/styles/tools/_sassMagic.scss')}";`,
-      },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "${path.resolve(
+                    __dirname,
+                    "./src/styles/tools/_sassMagic.scss"
+                )}";`,
+            },
+        },
     },
-  },
-})
+});
