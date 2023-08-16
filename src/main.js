9efe25480f1ac021847ca1396c4138a47aa55d5a
haopeng <haopeng138@gmail.com>
2023-08-16 22:38:19 +0200

    change docs

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import 'normalize.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/icon/iconfont.css'
+
+document.addEventListener("visibilitychange", function () {
+    if (document.visibilityState === "visible") {
+        // When the tab becomes visible, change the title
+        document.title = "Welcome back!";
+        setTimeout(() => {
+            document.title = "Haopeng's Portofolio";
+        }, 2000);
+    } else {
+        // When the tab becomes hidden, change the title
+        document.title = "(T⌓T) Come back pls!";
+    }
+});
+
 createApp(App).use(i18n).mount('#app')
