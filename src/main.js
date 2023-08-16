cb0c04fe61935add61e15a04c49cd702ed717eb1
haopeng <haopeng138@gmail.com>
2023-08-16 22:19:34 +0200

    try to fix img route problem

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import i18n from './i18n'
-import 'normalize.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles/icon/iconfont.css'
-createApp(App).use(i18n).mount('#app')
