8e29032f1fa73500f7cabf63c246b92b4c902296
haopeng <haopeng138@gmail.com>
2023-08-16 22:14:21 +0200

    try to fix img route problem

--- src/js/changeTitle.js
+++ src/js/changeTitle.js
@@ -1,30 +1,17 @@
-const title = document.querySelector('title');
+const title = document.querySelector("title");
 const changeTitle = (newTitle) => {
     title.innerText = newTitle;
 };
 
-// if user change tabs in browser change title
-// window.addEventListener('blur', () => {
-//     changeTitle('Come back!');
-// });
-
-document.addEventListener("visibilitychange", function() {
+document.addEventListener("visibilitychange", function () {
     if (document.visibilityState === "visible") {
-      // When the tab becomes visible, change the title
-      document.title = "Welcome back!";
-      setTimeout(() => {
-        document.title = "Haopeng's Portofolio";
+        // When the tab becomes visible, change the title
+        document.title = "Welcome back!";
+        setTimeout(() => {
+            document.title = "Haopeng's Portofolio";
         }, 2000);
     } else {
-      // When the tab becomes hidden, change the title
-      document.title = "(T⌓T) Come back pls!";
+        // When the tab becomes hidden, change the title
+        document.title = "(T⌓T) Come back pls!";
     }
-  });
-
-// // if user come back to tab change title
-// window.addEventListener('focus', () => {
-//     changeTitle('Welcome back!');
-//     setTimeout(() => {
-//         changeTitle("Haopeng's Portofolio");
-//     },1000);
-// });
+});
